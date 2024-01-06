4d738f88ed75b5b41a917adb2d6857400eccc87c
MaciejWozniakowski <maciejwozniakowski02@gmail.com>
2024-01-06 13:48:18 +0100

    found a frame to test kalman filter

---
+++ kalmanFilter/videoTest.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+
+# load videos into openCV
+# capture = cv.VideoCapture("efficientnetb0_output_video.mp4")
+capture = cv.VideoCapture("efficientnetb5_output_video.mp4")
+desiredFrame = 200
+# capture = cv.VideoCapture("mobilenetv2_output_video.mp4")
+frames = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
+# while True:
+#    ret, img = capture.read()
+#    if ret == False:
+#        break
+#    cv.imshow("Image", img)
+#    if cv.waitKey(20) & 0xFF == ord("d"):  # key 'd' stops the video playback
+#        break
+
+
+# display frames one by one
+# for frame in range(frames):
+#    print(frame)
+#    ret, img = capture.read()
+#    if ret == False:
+#        break
+#    cv.imshow("Image", img)
+#    if cv.waitKey(20) & 0xFF == ord("d"):  # key 'd' stops the video playback
+#        break
+framesList = []
+for frame in range(frames):
+    ret, img = capture.read()
+    if ret == False:
+        break
+    framesList.append(img)
+
+
+cv.imshow("img", framesList[100])
+cv.waitKey()  # key 'd' stops the video playback
+
+capture.release()
+cv.destroyAllWindows()
